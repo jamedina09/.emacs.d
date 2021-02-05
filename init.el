657d68fd43140d908791f61111903df24dc0b125
jamedina09 <jamedina09@gmail.com>
2021-02-05 14:47:05 -0500

    change color of text/comments

--- init.el
+++ init.el
@@ -38,21 +38,21 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-dracula t) ;Iosvkem
-
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
-
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
 ;; Change highlight region color
 (set-face-attribute 'region nil :background "grey35")
+
+;(set-face-foreground 'font-lock-string-face "")
+(set-face-foreground 'font-lock-comment-face "gray63")
 
 
 ;;----------------------------------------------------------------------------
