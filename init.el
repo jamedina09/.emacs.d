566a1a6bf3a8ccbf5457ab1271f4dbb98d125fbd
jamedina09 <jamedina09@gmail.com>
2021-06-03 13:31:27 -0500

    change emacs version

--- init.el
+++ init.el
@@ -1427,17 +1427,3 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 ;;----------------------------------------------------------------------------
 ;;----------------------------------------------------------------------------
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(latex-preview-pane auctex-latexmk auctex flycheck-stan company-stan stan-mode poly-R grip-mode lsp-python-ms lsp-ui lsp-mode ess elpy org-bullets org-roam org-journal pdf-tools all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired all-the-icons-ivy-rich all-the-icons-ivy beacon highlight-indent-guides rainbow-mode rainbow-delimiters which-key dired-k undo-tree fix-word flycheck-pos-tip flycheck-tip flycheck popwin yasnippet-snippets yasnippet diff-hl magit company-emoji company counsel-projectile counsel drag-stuff ace-window fancy-battery doom-modeline smartparens dimmer projectile all-the-icons restart-emacs exec-path-from-shell goto-line-preview osx-trash transpose-frame dired-ranger dired-subtree dired-narrow neotree dashboard doom-themes use-package))
- '(pdf-tools-handle-upgrades nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
